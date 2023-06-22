c9d070365448c9655c541f95932a9b192bc93ec0
shikamusenge <shikamusenge720@gmail.com>
2023-06-22 09:03:48 +0200

    adding Menu fanctionality

---
+++ js/app.js
@@ -0,0 +1,21 @@
+const menuBtn = document.querySelector(".menu");
+const closeBtn = document.querySelector("#close-btn");
+const linksDiv = document.querySelector("#navbar-links");
+const semBlack = document.querySelector("#sem-black");
+const ul = document.querySelector("#links-container");
+const links = ul.querySelectorAll("a");
+console.log(links);
+menuBtn.onclick = () => {
+  linksDiv.style.display = "block";
+  semBlack.className = "sem-black";
+  ul.id = "showMenu";
+};
+closeBtn.onclick = () => close();
+links.forEach((lnk) => {
+  lnk.onclick = () => close();
+});
+const close = () => {
+  linksDiv.style.display = "none";
+  semBlack.classList.remove("sem-black");
+  ul.id = "";
+};
